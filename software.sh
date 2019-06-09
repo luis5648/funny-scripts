c517aa79a6d4314ef90d41b66eb4310526c9742a
Luis <porta5648@gmail.com>
2019-06-09 13:32:00 -0500

    ides varios

--- software.sh
+++ software.sh
@@ -1,3 +1,20 @@
 apt install build-essential
 apt install geany
 apt install openjdk-9-jdk
+apt install git
+
+#instalar ides varios
+sudo snap install eclipse --classic
+#sudo snap install netbeans --classic
+#sudo snap install pycharm-educational --classic
+#sudo snap install intellij-idea-community --classic
+sudo snap install sublime-text --classic
+sudo snap install code --classic
+sudo snap install node --channel=10/stable --classic
+#sudo snap install atom --classic
+sudo snap install notepad-plus-plus
+sudo snap install ruby --classic
+#sudo snap install kotlin --classic
+
+#experimentales
+sudo snap install mysql --beta
